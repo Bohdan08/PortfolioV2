eb8222715f3375d56b8941e5d357eba1901724de
Bohdan881 <6228238-Bohdan881@users.noreply.gitlab.com>
2021-11-08 14:21:55 -0500

    feat: added state

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "gatsby";
 import styled from "styled-components";
 import { MobileNav } from "./index";
